97ac24062396767a1f67c51c011530cfe672061a
Artemii Yanushevskyi <artemii.yanushevskyi@gmail.com>
2022-10-21 15:53:26 +0300

    Add input and output parameters

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-console.log('Hello world')
+const core = require('@actions/core')
+
+try {
+    const appId = core.getInput('app-id')
+    console.log(`app_id: ${appId}`)
+    const workflowId = core.getInput('workflow-id')
+    console.log(`workflow_id: ${workflowId}`)
+    core.setOutput('build-id', 'test-build-id')
+} catch (error) {
+    core.setFailed(error.message)
+}
 
